f05d545f5c77d1b5e01651d8095eaa2ec351c782
Alexander Knight <Aebian@users.noreply.github.com>
2016-01-16 10:23:18 -0500

    Create AebianTheme.i18n.php

---
+++ AebianTheme.i18n.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Internationalization file for Aebian Wiki Theme.
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+$messages = array();
+
+/** English
+ * @author Alexander Knight
+ */
+$messages['en'] = array(
+	'AebianTheme-aebiantheme' => "Aebian Theme",
+	'myskin-desc' => "The Main Theme for the Aebian Wiki.",
+);
+?>
