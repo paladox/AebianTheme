d2a358b6359cac0f8db24dd23d8bfacaa50e1837
Paladox <thomasmulhall410@yahoo.com>
2016-01-16 15:39:08 +0000

    Update skin to be compatible with MediaWiki 1.26+
    Fixes some issues.
    
    As this skin extends vector you need to load vector before this skin in
    LocalSettings.php
    
    So it would be like this
    
    require_once "$IP/skins/Vector/Vector.php";
    
    require_once "$IP/skins/AebianTheme/AebianTheme.php";
    
    Change-Id: I41451bbdc135d584638b117415f0b93ecee09fe1

--- AebianTheme.php
+++ AebianTheme.php
@@ -8,25 +8,16 @@
  * @license https://Aebian.org/licenses/nm
  */
 
-if( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
-
-$wgExtensionCredits['skin'][] = array (
-	'path' => __FILE__,
-	'name' => 'Aebian Wiki Theme',
-	'url' => "https://Aebian.org/",
-	'author' => 'Alexander Knight',
-	'descriptionmsg' => 'mywiki-desc',
-);
-
-$wgValidSkinNames['AebianTheme'] = 'AebianTheme';
-$wgAutoloadClasses['SkinAebianTheme'] = dirname(__FILE__).'/AebianTheme.skin.php';
-$wgExtensionMessagesFiles['AebianTheme'] = dirname(__FILE__).'/AebianTheme.i18n.php';
-
-$wgResourceModules['skins.AebianTheme'] = array(
-	'styles' => array(
-		'AebianTheme/css/screen.css' => array( 'media' => 'screen' ),
-	),
-	'remoteBasePath' => &$GLOBALS['wgStylePath'],
-	'localBasePath' => &$GLOBALS['wgStyleDirectory'],
-);
-?>
+if ( function_exists( 'wfLoadSkin' ) ) {
+	wfLoadSkin( 'AebianTheme' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['AebianTheme'] = __DIR__ . '/i18n';
+	/* wfWarn(
+		'Deprecated PHP entry point used for AebianTheme skin. ' .
+		'Please use wfLoadSkin instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return true;
+} else {
+	die( 'This version of the AebianTheme skin requires MediaWiki 1.25+' );
+}
